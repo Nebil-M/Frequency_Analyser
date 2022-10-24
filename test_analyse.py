1c8087de32a82504f86a9b33f5f7541d54222d0b
Nebil <hiachi1118@gmail.com>
2022-10-23 23:15:24 -0400

    initial

--- test_analyse.py
+++ test_analyse.py
@@ -3,20 +3,20 @@ from collections import Counter
 
 
 def main():
-    with open("Texts/Eng/test.txt", encoding='utf-8') as file_object:
+    with open("Texts/ES/test.txt", encoding='utf-8') as file_object:
         book = file_object.read()
     print_non_aplha_eng(book)
     print_contractions_eng(book)
 
 def print_non_aplha_eng(text):
-    a = EnglishAnalyser(text)
+    a = SpanishAnalyser(text)
     non_num = [i for i in a.unique_lemma_freq_list if i[0].isalpha() == False]
     print(non_num)
     print(a.unique_lemmas, a.total_words)
 
 
 def print_contractions_eng(text):
-    a = EnglishAnalyser(text)
+    a = SpanishAnalyser(text)
     non_num = [i for i in a.txt.split() if i.isalpha() == False]
     print(Counter(non_num))
 
