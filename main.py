125ed04a90d016935961af72704091b4bea8408d
Nebil <hiachi1118@gmail.com>
2022-11-11 16:05:32 -0500

    Make some corpus, arranged analysis

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 def corpus_files(lang):
     corpus = ""
-    for textfile in os.scandir(f"Texts/{lang}"):
+    for textfile in os.scandir(f"Texts/{lang}/CorpusES"):
         with open(textfile, 'r', encoding="utf-8") as file:
             text = file.read()
         corpus += text 
@@ -11,12 +11,12 @@ def corpus_files(lang):
 
   
 def main():
-    book = corpus_files("Eng")
-    a = EnglishAnalyser(book)
+    book = corpus_files("ES")
+    a = SpanishAnalyser(book)
     print(a.unique_lemma_freq_list[:100])
-    print(a.unique_lemmas, a.total_words)
-    print(len((a.n_freq_lemmas(1))))
-    print(a.word_coverage(2553))
+    print("Unique lemmas:",a.unique_lemmas, "Total words:",a.total_words)
+    print("Words only happening once:",len((a.n_freq_lemmas(1))))
+    print("coverage of 100 words:",a.word_coverage(100))
 
 
 if __name__ == "__main__":
--- messing.py
+++ messing.py
@@ -1,8 +1,16 @@
 from analyse import *
 import timeit
 
+
 def timing(func):
     print(timeit.timeit(func, number=1))
 
 
+with open('Texts/ES/testb.txt', 'r', encoding="utf-8") as file:
+    text = file.read()
 
+a = SpanishAnalyser(text)
+print(a.unique_lemma_freq_list[:100])
+print("Unique lemmas:", a.unique_lemmas, "Total words:", a.total_words)
+print("Words only happening once:", len((a.n_freq_lemmas(1))))
+print("coverage of 100 words:", a.word_coverage(100))
