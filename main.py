692c17f00d275fa76d00d7c19977214c7a1be0e7
Nebil Mohammed <108494224+Nebil-M@users.noreply.github.com>
2022-11-15 19:41:35 +0000

    Created notes

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from analyse import *
-from io_functions_analyse import timing, load
+from io_functions_analyse import load, timing
+
 
 def main():
-
+    index = 12
     a = load("FR")
     coverage = 20000
-    print(a.unique_lemma_freq_list[:100])
+    print(a.unique_lemma_freq_list[:10])
     print("Unique lemmas:", a.unique_lemmas, "Total words:", a.total_words)
     print(f"coverage of {coverage} words:", a.word_coverage(coverage))
     print("Words only happening once:", len((a.n_freq_lemmas(1))))
-    print(a.n_freq_lemmas(2,1)[:100])
+    print(a.n_freq_lemmas(index,index-1)[:10])
 
 
 if __name__ == "__main__":
