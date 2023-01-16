2e023d97869bf567f9546d276de717a6c585407a
Nebil <hiachi1118@gmail.com>
2023-01-16 11:37:25 -0500

    Graphs

--- freq_graph.py
+++ freq_graph.py
@@ -1,25 +1,11 @@
-import matplotlib.pyplot as plt
-import numpy as np
 from io_functions_analyse import*
 
-# MATPLOTLIB
 
-def graph(*objs, step=100, title=None, log=True):
-    fig, ax = plt.subplots()
-
-    ax.set_xscale("log") if log else None
-    ax.set_xlabel('Unique Lemmas', fontsize=12)
-    ax.set_ylabel('Percent Coverage', fontsize=12)
-    ax.set_title(title, fontsize=20)
-    ax.grid(True)
-
-    for obj in objs:
-        x = np.arange(0, obj.unique_lemmas, step)
-        y = list(map(lambda i: obj.word_coverage(i)[1], x))
-        ax.plot(x, y, label=str(type(obj).__name__)[-9::-1][::-1])
-    ax.legend()
+f = load_corpus('project_data/Analyser_Objects/Eng')
+e = load_corpus('project_data/Analyser_Objects/FR')
+en = load_corpus('project_data/Analyser_Objects/ES')
 
 
-timing(lambda: graph(fr, es, en, step=1, title="Percent coverage vs Unique lemmas", log=True))
 
-plt.show()
+timing(lambda: graph(f, e, en, step=1, title="Percent coverage vs Unique lemmas", log=False))
+
--- io_functions_analyse.py
+++ io_functions_analyse.py
@@ -4,6 +4,25 @@ import pickle
 import re
 import os
 import cProfile
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def graph(*objs, step=100, title=None, log=True):
+    fig, ax = plt.subplots()
+
+    ax.set_xscale("log") if log else None
+    ax.set_xlabel('Unique Lemmas', fontsize=12)
+    ax.set_ylabel('Percent Coverage', fontsize=12)
+    ax.set_title(title, fontsize=20)
+    ax.grid(True)
+
+    for obj in objs:
+        x = np.arange(0, obj.unique_lemmas, step)
+        y = list(map(lambda i: obj.word_coverage(i)[1], x))
+        ax.plot(x, y, label=str(type(obj).__name__)[-9::-1][::-1])
+    ax.legend()
+    plt.show()
 
 
 def timing(func):
